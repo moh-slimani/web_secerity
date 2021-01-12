229422262919fb405f960dd884162684058d7abe
Mohamed Slimani <slimani.m1993@gmail.com>
2021-01-13 00:13:44 +0100

    remove usless links

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -54,9 +54,9 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo URL?>analyses">
-                            <i class="fas fa-vial text-yellow"></i>
-                            <span class="nav-link-text">Analyses</span>
+                        <a class="nav-link" href="<?php echo  URL?>results">
+                            <i class="fas fa-vials text-info"></i>
+                            <span class="nav-link-text">Results</span>
                         </a>
                     </li>
                     <li class="nav-item">
@@ -66,9 +66,9 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="<?php echo  URL?>results">
-                            <i class="fas fa-vials text-info"></i>
-                            <span class="nav-link-text">Results</span>
+                        <a class="nav-link" href="<?php echo URL?>analyses">
+                            <i class="fas fa-vial text-yellow"></i>
+                            <span class="nav-link-text">Analyses</span>
                         </a>
                     </li>
                 </ul>
@@ -83,7 +83,7 @@
         <div class="container-fluid">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Search form -->
-                <form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
+                <!--<form class="navbar-search navbar-search-light form-inline mr-sm-3" id="navbar-search-main">
                     <div class="form-group mb-0">
                         <div class="input-group input-group-alternative input-group-merge">
                             <div class="input-group-prepend">
@@ -96,11 +96,10 @@
                             aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
-                </form>
+                </form>-->
                 <!-- Navbar links -->
                 <ul class="navbar-nav align-items-center  ml-md-auto ">
                     <li class="nav-item d-xl-none">
-                        <!-- Sidenav toggler -->
                         <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin"
                              data-target="#sidenav-main">
                             <div class="sidenav-toggler-inner">
@@ -110,29 +109,26 @@
                             </div>
                         </div>
                     </li>
-                    <li class="nav-item d-sm-none">
+                    <!--<li class="nav-item d-sm-none">
                         <a class="nav-link" href="#" data-action="search-show" data-target="#navbar-search-main">
                             <i class="ni ni-zoom-split-in"></i>
                         </a>
-                    </li>
-                    <li class="nav-item dropdown">
+                    </li>-->
+                    <!--<li class="nav-item dropdown">
                         <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                            aria-expanded="false">
                             <i class="ni ni-bell-55"></i>
                         </a>
                         <div class="dropdown-menu dropdown-menu-xl  dropdown-menu-right  py-0 overflow-hidden">
-                            <!-- Dropdown header -->
                             <div class="px-3 py-3">
                                 <h6 class="text-sm text-muted m-0">You have <strong class="text-primary">13</strong>
                                     notifications.</h6>
                             </div>
-                            <!-- List group -->
                             <div class="list-group list-group-flush">
                                 <a href="#!" class="list-group-item list-group-item-action">
                                     <div class="row align-items-center">
                                         <div class="col-auto">
-                                            <!-- Avatar -->
-                                            <img alt="Image placeholder" src="<?php echo URL; ?>img/theme/team-1.jpg"
+                                            <img alt="Image placeholder" src="<?php /*echo URL; */?>img/theme/team-1.jpg"
                                                  class="avatar rounded-circle">
                                         </div>
                                         <div class="col ml--2">
@@ -151,8 +147,7 @@
                                 <a href="#!" class="list-group-item list-group-item-action">
                                     <div class="row align-items-center">
                                         <div class="col-auto">
-                                            <!-- Avatar -->
-                                            <img alt="Image placeholder" src="<?php echo URL; ?>img/theme/team-2.jpg"
+                                            <img alt="Image placeholder" src="<?php /*echo URL; */?>img/theme/team-2.jpg"
                                                  class="avatar rounded-circle">
                                         </div>
                                         <div class="col ml--2">
@@ -171,8 +166,7 @@
                                 <a href="#!" class="list-group-item list-group-item-action">
                                     <div class="row align-items-center">
                                         <div class="col-auto">
-                                            <!-- Avatar -->
-                                            <img alt="Image placeholder" src="<?php echo URL; ?>img/theme/team-3.jpg"
+                                            <img alt="Image placeholder" src="<?php /*echo URL; */?>img/theme/team-3.jpg"
                                                  class="avatar rounded-circle">
                                         </div>
                                         <div class="col ml--2">
@@ -191,8 +185,7 @@
                                 <a href="#!" class="list-group-item list-group-item-action">
                                     <div class="row align-items-center">
                                         <div class="col-auto">
-                                            <!-- Avatar -->
-                                            <img alt="Image placeholder" src="<?php echo URL; ?>img/theme/team-4.jpg"
+                                            <img alt="Image placeholder" src="<?php /*echo URL; */?>img/theme/team-4.jpg"
                                                  class="avatar rounded-circle">
                                         </div>
                                         <div class="col ml--2">
@@ -211,8 +204,7 @@
                                 <a href="#!" class="list-group-item list-group-item-action">
                                     <div class="row align-items-center">
                                         <div class="col-auto">
-                                            <!-- Avatar -->
-                                            <img alt="Image placeholder" src="<?php echo URL; ?>img/theme/team-5.jpg"
+                                            <img alt="Image placeholder" src="<?php /*echo URL; */?>img/theme/team-5.jpg"
                                                  class="avatar rounded-circle">
                                         </div>
                                         <div class="col ml--2">
@@ -229,12 +221,11 @@
                                     </div>
                                 </a>
                             </div>
-                            <!-- View all -->
                             <a href="#!" class="dropdown-item text-center text-primary font-weight-bold py-3">View
                                 all</a>
                         </div>
-                    </li>
-                    <li class="nav-item dropdown">
+                    </li>-->
+                    <!--<li class="nav-item dropdown">
                         <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
                            aria-expanded="false">
                             <i class="ni ni-ungroup"></i>
@@ -279,7 +270,7 @@
                                 </a>
                             </div>
                         </div>
-                    </li>
+                    </li>-->
                 </ul>
                 <ul class="navbar-nav align-items-center  ml-auto ml-md-0 ">
                     <li class="nav-item dropdown">
